9bf83cad6ab05a0b643f17fe3d8853786d895eff
Lucas Oliveira <lucas@fptbb.com>
2025-06-09 19:15:13 -0300

    Update configure_system.sh

--- build_files/configure_system.sh
+++ build_files/configure_system.sh
@@ -5,4 +5,7 @@ systemctl enable podman.socket
 systemctl disable NetworkManager-wait-online.service
 
 # Configure plymouth branding
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
 plymouth-set-default-theme -r
+dracut --force --regenerate-all --verbose --tmpdir /var/tmp
