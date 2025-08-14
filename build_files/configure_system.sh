acafc09a34a1ec128fae38d5448895bc4738f7c5
Lucas Oliveira <lucas@fptbb.com>
2025-08-14 14:19:22 -0300

    Update configure_system.sh

--- build_files/configure_system.sh
+++ build_files/configure_system.sh
@@ -2,15 +2,15 @@
 set -ouex pipefail
 
 IMAGE_INFO="/usr/share/ublue-os/image-info.json"
-IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/fptbb/fp-os"
 
 # image-info File
-sed -i 's/"image-name": [^,]*/"image-name": "'"$IMAGE_NAME"'"/' $IMAGE_INFO
-sed -i 's/"image-vendor": [^,]*/"image-vendor": "'"$IMAGE_VENDOR"'"/' $IMAGE_INFO
+sed -i 's/"image-name": [^,]*/"image-name": "'"fp-os"'"/' $IMAGE_INFO
+sed -i 's/"image-vendor": [^,]*/"image-vendor": "'"fptbb"'"/' $IMAGE_INFO
 sed -i 's|"image-ref": [^,]*|"image-ref": "'"$IMAGE_REF"'"|' $IMAGE_INFO
 
 # OS Release File
-sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+sed -i "s/^VARIANT_ID=.*/VARIANT_ID=fp-os/" /usr/lib/os-release
 sed -i "s/^NAME=.*/NAME="'"Fp OS"'"/" /usr/lib/os-release
 sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME="'"Fp OS (From Bazzite)"'"/" /usr/lib/os-release
 sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="'"fp-pc"'"/" /usr/lib/os-release
