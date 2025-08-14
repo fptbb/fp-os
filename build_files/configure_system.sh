c0b7e006dd49dc015d9319caf1caa994a9fc612f
Lucas Oliveira <lucas@fptbb.com>
2025-08-14 14:12:32 -0300

    Update configure_system.sh

--- build_files/configure_system.sh
+++ build_files/configure_system.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -ouex pipefail
 
+IMAGE_INFO="/usr/share/ublue-os/image-info.json"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
+
+# image-info File
+sed -i 's/"image-name": [^,]*/"image-name": "'"$IMAGE_NAME"'"/' $IMAGE_INFO
+sed -i 's/"image-vendor": [^,]*/"image-vendor": "'"$IMAGE_VENDOR"'"/' $IMAGE_INFO
+sed -i 's|"image-ref": [^,]*|"image-ref": "'"$IMAGE_REF"'"|' $IMAGE_INFO
+
+# OS Release File
+sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+sed -i "s/^NAME=.*/NAME="'"Fp OS"'"/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME="'"Fp OS (From Bazzite)"'"/" /usr/lib/os-release
+sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="'"fp-pc"'"/" /usr/lib/os-release
+sed -i "s/^HOME_URL=.*/HOME_URL="'"https:\/\/os.fpt.icu"'"/" /usr/lib/os-release
+
 # Get Bazzite's pinned kernel version, export it for build
 export KERNEL_VERSION=$(curl -fsSL \
         https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/.github/workflows/build.yml | \
