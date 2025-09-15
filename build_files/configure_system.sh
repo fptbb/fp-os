c4858626fb6c0b1b578a89de4e2f745fabdd2554
Lucas Oliveira <lucas@fptbb.com>
2025-09-15 16:10:41 -0300

    Update configure_system.sh

--- build_files/configure_system.sh
+++ build_files/configure_system.sh
@@ -12,7 +12,7 @@ sed -i 's|"image-ref": [^,]*|"image-ref": "'"$IMAGE_REF"'"|' $IMAGE_INFO
 # OS Release File
 sed -i "s/^VARIANT_ID=.*/VARIANT_ID=fp-os/" /usr/lib/os-release
 sed -i "s/^NAME=.*/NAME="'"Fp OS"'"/" /usr/lib/os-release
-sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME="'"Fp OS (From Bazzite)"'"/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME="'"Fp OS"'"/" /usr/lib/os-release
 sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="'"fp-pc"'"/" /usr/lib/os-release
 sed -i "s/^HOME_URL=.*/HOME_URL="'"https:\/\/os.fpt.icu"'"/" /usr/lib/os-release
 
